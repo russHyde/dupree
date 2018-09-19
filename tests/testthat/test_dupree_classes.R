4780ec9356c1b64bee0c946bfb578d1a0c5954a0
Russell Hyde <russ_hyde@hotmail.com>
2018-09-19 18:59:45 +0100

    fixes failing tests for EnumeratedCodeTable
    Added tidyr to Suggests. `tidyr::unnest` is used in
    the unit-tests but not the package-code.

--- tests/testthat/test_dupree_classes.R
+++ tests/testthat/test_dupree_classes.R
@@ -6,12 +6,17 @@ context("Tests for classes in `dupree` package")
 
 #' Since you can't compare two `tbl_df` objects when they contain a list as a
 #' column using expect_equal or all.equal
+#'
+#' @importFrom   tidyr         unnest
+#'
 expect_equal_listy_tbl <- function(object, expected, ...) {
-  expect_equal(
-    object = tidyr::unnest(object),
-    expected = tidyr::unnest(expected),
-    ...
-  )
+  if (requireNamespace("tidyr", quietly = TRUE)) {
+    expect_equal(
+      object = tidyr::unnest(object),
+      expected = tidyr::unnest(expected),
+      ...
+    )
+  }
 }
 
 ###############################################################################
