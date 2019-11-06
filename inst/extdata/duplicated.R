f4f9d7dc73d06f0f3e5527d4d1ed0a034127f6ab
Russell Hyde <russ.hyde.data@gmail.com>
2019-11-06 10:25:36 +0000

    expand docs for `dupree::dupree` and add example
    Added:
    - Description / Details section added for `dupree`
    - `value` tag with a description of the data-frame that is returned
    - `seealso` tags to `dupree_dir` and `dupree_package`, referring them to
      `dupree`
    - A system.file example to inst/extdata that is used in the examples for
      dupree
    
    Also added CRAN-RELEASE file to Rbuildignore

---
+++ inst/extdata/duplicated.R
@@ -0,0 +1,25 @@
+# Example script for illustrating code duplication
+library(dplyr)
+data(diamonds)
+
+diamonds %>%
+  filter(clarity %in% c("SI1", "SI2")) %>%
+  group_by(color) %>%
+  summarise(m_price = mean(price), sd_price = sd(price))
+
+diamonds %>%
+  filter(cut >= "Very Good") %>%
+  group_by(color) %>%
+  summarise(m_price = mean(price), sd_price = sd(price))
+
+
+# note that dupree can't tell that the following code is logically
+# the same as the preceding code
+summarise(
+  group_by(
+    filter(diamonds, cut >= "Very Good"),
+    color
+  ),
+  sd_price = sd(price),
+  m_price = mean(price)
+)
