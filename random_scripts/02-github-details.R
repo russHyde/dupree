dc019de0f6bba08c4460dbdd6e21903200b29b92
Russell Hyde <russ.hyde.data@gmail.com>
2019-11-27 10:34:26 +0000

    implement extraction of github URLs

--- random_scripts/02-github-details.R
+++ random_scripts/02-github-details.R
@@ -1,18 +1,80 @@
 ###############################################################################
 
+get_repo_from_comma_sepd_string <- function(x) {
+  # input could be "http[s]://[www.]github.com/<user>/<repo>[/issues], some-other-url"
+
+  # For each entry in the vector of strings, return the first URL that is of
+  # the form
+  # "http[s]://[www.]github.com/<user>/repo[/<subdir>]"
+  # But strip off the optional <subdir> section (eg, issues)
+
+  stringr::str_extract(
+    string = x,
+    pattern = "(https*://(www\\.)*github.com/[^/]+/[^/]+)"
+  )
+}
+
+test_get_repo <- function() {
+  if (!requireNamespace("testthat")) {
+    message("testthat should be installed")
+    return()
+  }
+  testthat::test_that("repo_parser_works - single URLs", {
+    repo <- "github.com/abc/123"
+    ht <- c("https://", "http://")
+    www <- c("", "www.")
+    suffixes <- c("", "/", "/issues")
+
+    grid <- expand.grid(ht, www, repo, suffixes)
+    input_url <- do.call(paste0, grid)
+    expected_url <- do.call(paste0, grid[1:3])
+    testthat::expect_equal(
+      get_repo_from_comma_sepd_string(input_url),
+      expected_url
+    )
+  })
+}
+
 define_repositories <- function(pkg_table, repo_dir) {
-  stop("TODO: define_repositories() function")
   # Define a table containing package-name, remote-GH-url and
   # local-repo-filepath (based on the filtered cran table)
+
+  # URLs are stored as a ", "-separated string within fields 'url' and
+  # 'bug_reports'
+
+  define_remote <- function() {
+    get_repo_from_comma_sepd_string(
+      paste(pkg_table$url, pkg_table$bug_reports, sep = ", ")
+    )
+  }
+  define_local <- function() {
+    file.path(repo_dir, pkg_table$package)
+  }
+
+  tibble::tibble(
+    package = pkg_table$package,
+    remote_repo = define_remote(),
+    local_repo = define_local()
+  )
+}
+
+###############################################################################
+
+run_tests <- function() {
+  message("Running tests ...")
+  test_get_repo()
 }
 
 ###############################################################################
 
 run_script <- function(cran_details_file, repo_dir, results_file) {
+  message("Running script ...")
   # Converts a CRAN table that only contains github-hosted packages into a
   # table containing  (package-name, remote-repo, local-repo) paths.
 
-  dev_pkg_table <- readr::read_tsv(cran_details_file)
+  dev_pkg_table <- readr::read_tsv(
+    cran_details_file, col_types = cols(.default = "c")
+  )
 
   repo_details <- define_repositories(dev_pkg_table, repo_dir)
 
@@ -27,7 +89,9 @@ source("config.R")
 ###############################################################################
 
 # pkgs require for running the script (not the packages that are analysed here)
-load_packages(c("dplyr", "magrittr", "readr", "tibble"))
+load_packages(c("dplyr", "magrittr", "readr", "stringr", "tibble"))
+
+run_tests()
 
 run_script(
   cran_details_file = config[["cran_details_file"]],
