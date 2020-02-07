5bbcf480b3a191c81ccfa222f893217ba1522931
Russell Hyde <russ_hyde@hotmail.com>
2020-02-07 17:48:25 +0000

    linting

--- R/dupree_classes.R
+++ R/dupree_classes.R
@@ -34,7 +34,10 @@ methods::setClass("EnumeratedCodeTable", slots = list(blocks = "tbl_df"))
   }
 }
 
-methods::setValidity("EnumeratedCodeTable", .is_enumerated_code_table)
+methods::setValidity(
+  "EnumeratedCodeTable",
+  function(object) .is_enumerated_code_table(object)
+)
 
 ###############################################################################
 
@@ -94,9 +97,12 @@ methods::setMethod(
 #'
 #' @noRd
 #'
+
+# nocov start
 methods::setGeneric("find_best_matches", function(x, ...) {
   methods::standardGeneric("find_best_matches")
 })
+# nocov end
 
 #' `find_best_matches` between code blocks in an `EnumeratedCodeTable`
 #'
