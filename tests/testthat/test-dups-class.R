5e11595f1f348a5a718b674726e62e20b5be98ed
Russ Hyde <russ.hyde.data@gmail.com>
2024-04-03 10:56:41 +0100

    test: print(dups, n = ...) works like printing a tibble

--- tests/testthat/test-dups-class.R
+++ tests/testthat/test-dups-class.R
@@ -28,3 +28,20 @@ test_that("`dups` object can be converted to `data.frame`", {
 test_that("non dups/data-frames can't be converted to dups", {
   expect_error(as_dups("NOT A data.frame or dups object"))
 })
+
+describe("printing a 'dups' object", {
+  dups <- as_dups(get_dups_tbl(file_a = paste0(letters, ".R")))
+
+  it("includes the first line in the output", {
+    expect_output(print(dups), regexp = "a\\.R")
+  })
+
+  it("respects print(tibble, n = ...)", {
+    # "z.R" is on the last line of the table, it shouldn't be visible by default
+    # because `print(tibble)` shows the first 10 lines for large tables
+    expect_output(print(dups), regexp = "[^z].\\R")
+    # But when 26 lines of the table are printed, then the file "z.R" should be
+    # seen
+    expect_output(print(dups, n = 26), regexp = "z\\.R")
+  })
+})
